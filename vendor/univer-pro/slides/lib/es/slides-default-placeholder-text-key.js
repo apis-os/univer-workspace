4d074cd36f9cbf649645c9a781f4958cc8751b88
Gabriel Paiva <gabrielcmpaiva@gmail.com>
2026-09-12 21:41:08 +0100

    chore(vendor): infer and named-LST-split slides, sheets-shape-ui, embed, boards-table-ui b12

---
+++ vendor/univer-pro/slides/lib/es/slides-default-placeholder-text-key.js
@@ -0,0 +1,10 @@
+import{ArrangeTypeEnum,BooleanNumber,CommandType,CustomCommandExecutionError,DependentOn,Disposable,DrawingTypeEnum,HorizontalAlign,ICommandService,IConfigService,IPermissionService,IResourceManagerService,IUndoRedoService,IUniverInstanceService,Inject,Injector,LocaleType,PermissionStatus,Plugin,Tools,UnitModel,UniverInstanceType,VerticalAlign,createIdentifier,createParagraphId,generateRandomId,getDrawingOrderIndex,merge,normalizeDrawingOrderIndex,sequenceExecute,shallowEqual}from'@univerjs/core';
+import{UnitDrawingService}from'@univerjs/drawing';
+import{IShapeHostAdapterRegistry,ShapeFillEnum,ShapeLineTypeEnum,ShapeModel,ShapeTextAutoFitType,ShapeTextWrapType,ShapeTypeEnum,UniverShapePlugin,applyDocumentToShapeText,applySmartArtOperation,canApplyShapeFormulaLastValue,convertSmartArtToShapes,createDefaultInsertedShapeData,createUniqueShapeName,isConnectorShape,isSmartArtShapeData,normalizeShapeTextData}from'@univerjs-pro/engine-shape';
+import{transformObjectOutOfGroup}from'@univerjs/engine-render';
+import{UnitAction,UnitObject}from'@univerjs/protocol';
+import{BehaviorSubject,Subject,map,merge as mergeLocal,mergeMap}from'rxjs';
+import{UniverLicensePlugin}from'@univerjs-pro/license';
+const Jr={'title':"slides.slide.placeholder.text.title",'centerTitle':"slides.slide.placeholder.text.title",'subtitle':"slides.slide.placeholder.text.subtitle",'body':'slides.slide.placeholder.text.body','text':"slides.slide.placeholder.text.text",'header':"slides.slide.placeholder.text.header",'footer':'slides.slide.placeholder.text.footer','date':"slides.slide.placeholder.text.date",'slideNumber':"slides.slide.placeholder.text.slideNumber"};function Yr(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D462316){return Jr[var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D462316];}
+
+export { Yr as resolveDefaultPlaceholderTextKey };
---
+++ vendor/univer-pro/slides/lib/es/slides-slide-element-permission-object-id.js
@@ -0,0 +1,11 @@
+import{ArrangeTypeEnum,BooleanNumber,CommandType,CustomCommandExecutionError,DependentOn,Disposable,DrawingTypeEnum,HorizontalAlign,ICommandService,IConfigService,IPermissionService,IResourceManagerService,IUndoRedoService,IUniverInstanceService,Inject,Injector,LocaleType,PermissionStatus,Plugin,Tools,UnitModel,UniverInstanceType,VerticalAlign,createIdentifier,createParagraphId,generateRandomId,getDrawingOrderIndex,merge,normalizeDrawingOrderIndex,sequenceExecute,shallowEqual}from'@univerjs/core';
+import{UnitDrawingService}from'@univerjs/drawing';
+import{IShapeHostAdapterRegistry,ShapeFillEnum,ShapeLineTypeEnum,ShapeModel,ShapeTextAutoFitType,ShapeTextWrapType,ShapeTypeEnum,UniverShapePlugin,applyDocumentToShapeText,applySmartArtOperation,canApplyShapeFormulaLastValue,convertSmartArtToShapes,createDefaultInsertedShapeData,createUniqueShapeName,isConnectorShape,isSmartArtShapeData,normalizeShapeTextData}from'@univerjs-pro/engine-shape';
+import{transformObjectOutOfGroup}from'@univerjs/engine-render';
+import{UnitAction,UnitObject}from'@univerjs/protocol';
+import{BehaviorSubject,Subject,map,merge as mergeLocal,mergeMap}from'rxjs';
+import{UniverLicensePlugin}from'@univerjs-pro/license';
+import { Qn } from "./slides-slide-unit-permission-actions.js";
+function tr(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D461922,var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D461923,var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D461924){return''+Qn+encodeURIComponent(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D461922)+'/'+encodeURIComponent(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D461923)+'/'+encodeURIComponent(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D461924);}
+
+export { tr as getSlideElementPermissionObjectId };
