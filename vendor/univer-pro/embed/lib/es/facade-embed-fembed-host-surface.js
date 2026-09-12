06b34d698a430339f3884bcfd1b6d9b84f597ffc
Gabriel Paiva <gabrielcmpaiva@gmail.com>
2026-09-12 21:52:41 +0100

    chore(vendor): infer and named-LST-split slides-exchange-client, embed, collaboration-client, docs-code, collaboration-embed, sheets-chart, sheets-shape, shape-editor b25

---
+++ vendor/univer-pro/embed/lib/es/facade-embed-fembed-host-surface.js
@@ -0,0 +1,6 @@
+import{CreateEmbedCommand,EMBED_CHILD_CREATE_OPTIONS,EmbedError,EmbedErrorCode,EmbedHostAdapterRegistryService,EmbedHostEntryEnum,EmbedModelService,EmbedReferencedUnitApiResolverRegistryService,EmbedReferencedUnitMaterializeService,IReferencedUnitManagerService,RemoveEmbedCommand,SetEmbedBoundsCommand,SetEmbedDisplayTargetCommand,SetEmbedSheetPlacementCommand,normalizeResourceRefLocator}from"@univerjs-pro/embed";
+import{FBase,FEnum,FUniver}from"@univerjs/core/facade";
+import{ICommandService,Inject,Injector,UniverInstanceType,generateRandomId}from"@univerjs/core";
+const C={DocBlock:EmbedHostEntryEnum.DocsCustomBlock,SheetTab:EmbedHostEntryEnum.SheetsSheetTab,SheetFloating:EmbedHostEntryEnum.SheetsFloatingObject,BaseTable:EmbedHostEntryEnum.BasesTableListBlock,SlidePage:EmbedHostEntryEnum.SlidesPageListBlock,SlideFloating:EmbedHostEntryEnum.SlidesFloatingObject,BoardFloating:EmbedHostEntryEnum.BoardsFloatingObject};
+
+export { C as FEmbedHostSurface };
