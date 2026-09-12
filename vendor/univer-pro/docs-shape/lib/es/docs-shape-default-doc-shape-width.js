6f350b6b72494fdbcb17cdeea1159e6aacac0227
Gabriel Paiva <gabrielcmpaiva@gmail.com>
2026-09-12 21:51:54 +0100

    chore(vendor): infer and named-LST-split docs-callout-ui, slides-chart, edit-history-ui, docs-shape, boards-chart, engine-chart, docs-history-ui, slides-table b19

---
+++ vendor/univer-pro/docs-shape/lib/es/docs-shape-default-doc-shape-width.js
@@ -0,0 +1,8 @@
+import{IConnectorShapeHostAdapter,IShapeHostAdapterRegistry,ShapeModel,UniverShapePlugin,canApplyShapeFormulaLastValue,createDefaultInsertedShapeData,isConnectorShape}from"@univerjs-pro/engine-shape";
+import{ArrangeTypeEnum,BooleanNumber,CommandType,DependentOn,Disposable,DrawingTypeEnum,ICommandService,IConfigService,IResourceManagerService,IUndoRedoService,IUniverInstanceService,Inject,Injector,JSONX,Plugin,Tools,UniverInstanceType,WrapTextType,generateRandomId,getParagraphContentStartOffset,merge,touchDependencies}from"@univerjs/core";
+import{IDocDrawingAdapterService,InsertDocDrawingCommand,RemoveDocDrawingCommand,SetDocDrawingArrangeCommand,TextWrappingStyle,UniverDocsDrawingPlugin,UpdateDrawingDocTransformCommand,WRAPPING_STYLE_TO_LAYOUT_TYPE}from"@univerjs/docs-drawing";
+import{RichTextEditingMutation,buildDocTransform,docDrawingPositionToTransform}from"@univerjs/docs";
+import{UniverLicensePlugin}from"@univerjs-pro/license";
+const N=160,ce=96;
+
+export { N as DEFAULT_DOC_SHAPE_WIDTH, ce as DEFAULT_DOC_SHAPE_HEIGHT };
