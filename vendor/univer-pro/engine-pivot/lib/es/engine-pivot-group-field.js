2a9e0784f8d886390de7d1b18ee7603437424f55
Gabriel Paiva <gabrielcmpaiva@gmail.com>
2026-09-12 21:40:16 +0100

    chore(vendor): infer and named-LST-split engine-pivot, docs-table-ui, sheets-pivot-ui, boards-mind-ui b11

---
+++ vendor/univer-pro/engine-pivot/lib/es/engine-pivot-group-field.js
@@ -0,0 +1,3 @@
+function I(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D463157){return var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D463157.getDataFieldType()!==1;}
+
+export { I as isBaseGroupField };
---
+++ vendor/univer-pro/engine-pivot/lib/es/engine-pivot-manual-pivot-filter.js
@@ -0,0 +1,3 @@
+function j(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D463069){return var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D463069.type===3;}
+
+export { j as isManualPivotFilter };
