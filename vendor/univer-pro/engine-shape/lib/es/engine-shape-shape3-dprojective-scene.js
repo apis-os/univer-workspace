bba0863c4b7d63dc40f4bf47e6a3d45f16488edb
Gabriel Paiva <gabrielcmpaiva@gmail.com>
2026-09-12 21:33:58 +0100

    chore(vendor): infer and named-LST-split engine-shape b01

---
+++ vendor/univer-pro/engine-shape/lib/es/engine-shape-shape3-dprojective-scene.js
@@ -0,0 +1,7 @@
+import{combineDrawingEffectFilter,createDrawingEffectFilter,expandDrawingEffectBounds}from'@univerjs/engine-render';
+import{BooleanNumber,DependentOn,HorizontalAlign,IConfigService,ImageSourceType,Inject,Injector,JSONX,Plugin,RichTextBuilder,RichTextValue,TextX,Tools,UniverInstanceType,VerticalAlign,createIdentifier,generateRandomId,getSingleDataStreamChange,merge,toDisposable}from'@univerjs/core';
+import{UniverLicensePlugin}from'@univerjs-pro/license';
+import{HostExternalReferenceModel}from'@univerjs-pro/engine-formula';
+function fn_L0_core_pred_routine_pure_O1_zalloc_nothrow_sig3B80(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D466448){return!!(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D466448&&var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D466448.camera['preset']!=="orthographicFront");}
+
+export { fn_L0_core_pred_routine_pure_O1_zalloc_nothrow_sig3B80 as isShape3DProjectiveScene };
---
+++ vendor/univer-pro/engine-shape/lib/es/engine-shape-smart-art-direction.js
@@ -0,0 +1,7 @@
+import{combineDrawingEffectFilter,createDrawingEffectFilter,expandDrawingEffectBounds}from'@univerjs/engine-render';
+import{BooleanNumber,DependentOn,HorizontalAlign,IConfigService,ImageSourceType,Inject,Injector,JSONX,Plugin,RichTextBuilder,RichTextValue,TextX,Tools,UniverInstanceType,VerticalAlign,createIdentifier,generateRandomId,getSingleDataStreamChange,merge,toDisposable}from'@univerjs/core';
+import{UniverLicensePlugin}from'@univerjs-pro/license';
+import{HostExternalReferenceModel}from'@univerjs-pro/engine-formula';
+function Yp(var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611598,var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611599){return var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611598.direction===var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611599?var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611598:{...var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611598,'direction':var_L0_core_endo_value_pure_O1_zalloc_nothrow_sig0D4611599};}
+
+export { Yp as setSmartArtDirection };
